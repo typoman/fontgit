8601b09e9d77e545bea35a69f85ad343879abbcd
Bahman Eslami <eslami.bahman@gmail.com>
2025-08-04 11:42:12 +0200

    [ver] raise error if can't find pyproject file on import

--- src/fontGit/__init__.py
+++ src/fontGit/__init__.py
@@ -1,4 +1,4 @@
-"""fontgit -- a package for loading UFO fonts inside a git repo at a spcific part of git history."""
+"""fontgit -- a package for loading UFO fonts inside a git repo at a specific part of git history."""
 from pathlib import Path
 from importlib.metadata import PackageNotFoundError, version
 import tomllib
@@ -6,11 +6,14 @@ import tomllib
 try:
     __version__ = version("fontGit")
 except PackageNotFoundError:
-    # The package is not installed, so we fall back to reading the version from pyproject.toml
     pyproject_path = Path(__file__).parent.parent.parent / "pyproject.toml"
-    with open(pyproject_path, "rb") as f:
-        pyproject_data = tomllib.load(f)
-    __version__ = pyproject_data["project"]["version"]
+    if pyproject_path.exists():
+        try:
+            with open(pyproject_path, "rb") as f:
+                pyproject_data = tomllib.load(f)
+            __version__ = pyproject_data["project"]["version"]
+        except Exception:
+            raise
 
 from fontGit.objects.font import FontGit
 
